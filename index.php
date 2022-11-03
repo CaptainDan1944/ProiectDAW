617d573e5139a2904a64ec706140ec79cf8aec0a
CaptainDan1944 <ungureanu.dan2003@gmail.com>
2022-11-03 23:45:05 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+    .Title{
+        color: blue;
+    }
+
+    .body, .head{
+        background-image: url("parchment-background.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: repeat;
+    }
+</style>
+    
+<head>
+<?php
+
+
+    echo "<h1 class='Title'>Royal Library of Stormwind</h1>";
+    echo "<h2 class='Subtitle'>Temple of Knowledge and Academy of Mystic Arts</h2>";
+
+
+?>
+</head>
+    
+<body>
+
+<?php
+
+
+    echo "Welcome to the library of Stormwind!";
+
+
+?>
+
+</body>
+</html>
